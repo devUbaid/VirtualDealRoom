20ec8fff7b197523c7f535794a43afa46b82bac6
devUbaid <devubaid493@gmail.com>
2025-04-04 02:17:55 +0530

    update1

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
-export const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000"
+export const API_URL = process.env.REACT_APP_API_URL || "https://virtualdealroom-backend.onrender.com"
+// export const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000"
 
